84325c6058584bc3a79b4730629ec39ed7f3d536
Scherbinsky Arseny 850505 <61932449+imperator1440@users.noreply.github.com>
2020-12-04 03:47:24 +0300

    Code Review

--- Scripts/ChestInteraction.cs
+++ Scripts/ChestInteraction.cs
@@ -3,16 +3,16 @@
 namespace Assets.Scripts {
     public class ChestInteraction : MonoBehaviour {
         [SerializeField]
-        private Animator m_animator;
+        private Animator m_animator; // Wrong naming ('m_'), private field must start with "_"
         [SerializeField]
-        private Material m_default;
+        private Material m_default; // Wrong naming ('m_'), private field must start with "_"
         [SerializeField]
-        private Material m_outlined;
+        private Material m_outlined; // Wrong naming ('m_'), private field must start with "_"
 
-        private const int m_gold = 50;
+        private const int m_gold = 50; // Wrong naming ('m_'), private field must start with "_"
 
-        private bool m_materialState;
-        private bool m_opened ;
+        private bool m_materialState; // Wrong naming ('m_'), private field must start with "_"
+        private bool m_opened ; // Wrong naming ('m_'), private field must start with "_"
 
         private void Start() {
             GetComponent<Renderer>().material = m_default;
