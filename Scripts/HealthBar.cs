bd2a40fd4bbb6cfd6e0d93f11be4288b71a1c0d6
Scherbinsky Arseny 850505 <61932449+imperator1440@users.noreply.github.com>
2020-12-04 03:56:38 +0300

    Code Review

--- Scripts/HealthBar.cs
+++ Scripts/HealthBar.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 namespace Assets.Scripts {
     public class HealthBar : MonoBehaviour {
-        [SerializeField] private Slider m_slider;
+        [SerializeField] private Slider m_slider; // Wrong naming ('m_'), private field must start with "_"
 
         public void SetMaxHealth(int health) {
             m_slider.maxValue = health;
