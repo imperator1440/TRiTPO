c81e343c19d833992005c6634ef7bf64ce18236e
Scherbinsky Arseny 850505 <61932449+imperator1440@users.noreply.github.com>
2020-12-04 03:54:24 +0300

    Code Review

--- Scripts/GameMaster.cs
+++ Scripts/GameMaster.cs
@@ -2,8 +2,8 @@
 
 public class GameMaster : MonoBehaviour
 {
-    private static GameMaster instance;
-    public Vector2 m_lastCheckPoint;
+    private static GameMaster instance; // Wrong naming ('m_'), private field must start with "_"
+    public Vector2 m_lastCheckPoint; // Wrong naming ('m_'), public field must start with uppercase letter and without prefix.
 
     private void Awake()
     {
