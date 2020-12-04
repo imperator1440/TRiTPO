3c239478869b623a58e5db4fe9b205f85395123d
Scherbinsky Arseny 850505 <61932449+imperator1440@users.noreply.github.com>
2020-12-04 03:49:05 +0300

    Code Review

--- Scripts/DoorInteraction.cs
+++ Scripts/DoorInteraction.cs
@@ -2,10 +2,10 @@
 
 namespace Assets.Scripts {
     public class DoorInteraction : MonoBehaviour {
-        [SerializeField] private Animator m_animator;
-        private bool m_state;
+        [SerializeField] private Animator m_animator; // Wrong naming ('m_'), private field must start with "_"
+        private bool m_state; // Wrong naming ('m_'), private field must start with "_"
 
-        public void HandleDoor() {
+        public void HandleDoor() { // Wrong (You need to change a bit name to correspond logic)
             m_animator.SetTrigger("Open");
             m_state = !m_state;
         }
